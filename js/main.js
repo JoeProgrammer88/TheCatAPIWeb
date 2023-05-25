b53f03ffa52da364f681a934bd7c458f63368eb4
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2023-05-25 11:18:45 -0700

    Added CatImage class for better intellisense support

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+class CatImage {
+}
 function loadData() {
     let url = "https://api.thecatapi.com/v1/images/search";
     fetch(url)
         .then(function (response) {
-        console.log(response);
-        return response.json();
+        return __awaiter(this, void 0, void 0, function* () {
+            console.log(response);
+            let jsonData = yield response.json();
+            return jsonData;
+        });
     })
         .then(function (data) {
         console.log(data);
--- ts/main.ts
+++ ts/main.ts
@@ -1,15 +1,32 @@
+/**
+ * This is a class that declares the attributes of
+ * cat photos received from TheCatAPI.
+ */
+class CatImage{
+    /** The API's id for the image */
+    id:string; 
+    /** The string of the url for the image */
+    url:string;
+    /** The width of the image in pixels */
+    width:number;
+    /** The height of the image in pixels */
+    height:number;
+}
+
 function loadData():void{
     // Get a random image from thecatapi.com
     let url = "https://api.thecatapi.com/v1/images/search";
     fetch(url)
     .then(
-        function(response:Response):Promise<JSON>{
+        async function(response:Response):Promise<CatImage[]>{
             console.log(response);
-            return response.json();
+            let jsonData = <CatImage[]>await response.json();
+
+            return jsonData;
         }
     )
     .then(
-        function(data:JSON){
+        function(data:CatImage[]){
             console.log(data);
             let catImage:HTMLImageElement = document.createElement("img");
             catImage.src = data[0].url; // data[0] because the API returns an array of images
